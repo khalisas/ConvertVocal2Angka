2630bd80a143864fff027f751b02db23de120bbb
khalisashiva <khalisa_shiva_28rpl@student-smktelkom-mlg.sch.id>
2022-10-29 00:22:59 +0700

    convert

---
+++ src/convertvocal2angka/ConvertVocal2Angka.java
@@ -0,0 +1,13 @@
+
+package convertvocal2angka;
+
+public class ConvertVocal2Angka {
+
+        private static void tampilJudul (String Identitas){
+        System.out.println("Identitas :" + Identitas);
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka \n");
+        }
+        String identitas = "Khalisa Shiva/X RPL 7/"
+    }
+    
+}
